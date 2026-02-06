38ab2faa4045fee0efda745d44569920c4c40236
theboldster <theboldster@gmail.com>
2026-02-06 16:08:21 +0100

    First Upload

---
+++ TimedSoundPlayer.ino
@@ -0,0 +1,21 @@
+// Older Nano Boards need to have the processor set to ATmega328P (Old Bootloader)
+
+// 1. Hook up RTC
+// 2. Hook up I2C Multiplexer
+// 3. Reattach RTC to MUX
+// 4. Hook up Optocoupler
+// 5. Hook up Speaker
+
+// On Nano / Uno R3
+// I2C SDA A4
+// I2C SCL A5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
